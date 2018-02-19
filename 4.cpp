09916da1632390a3079cf5043959d1c032d018ad
Unknown <federicogallego616@gmail.com>
2018-02-18 19:23:24 -0500

    Indicar suma

---
+++ 4.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+            int Num1,Num2,Num3;
+
+    cout << "ingrese numero 1: " ;
+    cin >> Num1;
+    cout << "Ingrese numero 2: ";
+    cin >> Num2;
+    cout << "Ingrese numero 3: ";
+    cin >> Num3;
+    if (Num1+Num2==Num3)
+    {
+         cout << "El tercer numero es la suma de los dos primeros." ;
+    }
+    else
+    {
+         cout << "El tercer numero NO es la suma de los dos primeros." ;
+    }
+    return 0;
+}
